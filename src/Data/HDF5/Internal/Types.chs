ee01cb89dae19f80dec391846e1f7e8810b8c74b
ocramz <zocca.marco@gmail.com>
2018-03-29 18:49:46 +0200

    adding functions

--- src/Data/HDF5/Internal/Types.chs
+++ src/Data/HDF5/Internal/Types.chs
@@ -21,3 +21,7 @@ type Htri = {#type htri_t #}
 -- | HDF5 native datatypes : https://support.hdfgroup.org/HDF5/doc/RM/PredefDTypes.html
   
 -- type HNativeDouble = {#type H5T_NATIVE_DOUBLE #}
+
+
+-- | flags
+
--- src/Data/HDF5/Lite.hs
+++ src/Data/HDF5/Lite.hs
@@ -5,16 +5,16 @@ module Data.HDF5.Lite where
 import           Data.Functor ((<$>))
 
 import Control.Monad.Catch (throwM, MonadThrow(..))
-import Control.Exception
+import Control.Exception (bracket)
 
-import           Foreign.C.String (peekCString, withCString)
-import           Foreign.C.Types
-import           Foreign.Marshal.Alloc (alloca)
+import Foreign.C.String (peekCString, withCString)
+import Foreign.C.Types
+import Foreign.Marshal.Alloc (alloca)
 import Foreign.Marshal.Array (withArray)
-import           Foreign.Ptr (Ptr)
+import Foreign.Ptr (Ptr)
 import Foreign.Storable
 
--- import GHC.Prim
+import GHC.Prim
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
@@ -38,6 +38,24 @@ C.include "<hdf5_hl.h>"
 
 
 
+
+
+
+-- * HDF5 file operations
+-- NB: each HDF5 physical file (e.g. "/usr/data/dataset0.h5" ) specifies a whole internal filesystem, the elements of which are what we call "files" here
+
+-- hid_t H5Fopen( const char *name, unsigned flags, hid_t fapl_id )
+fopenReadOnly :: String -> IO Hid
+fopenReadOnly name = withCString name $ \name_ -> 
+  [C.exp| hid_t{ H5Fopen( $(const char* name_), H5F_ACC_RDONLY, H5P_DEFAULT )}|]
+
+fopenRW :: String -> IO Hid
+fopenRW name = withCString name $ \name_ ->
+  [C.exp| hid_t{ H5Fopen( $(const char* name_), H5F_ACC_RDWR, H5P_DEFAULT )}|]
+
+
+
+-- * Dataset creation
 
 -- herr_t H5LTmake_dataset_double ( hid_t loc_id, const char *dset_name, int rank, const hsize_t *dims, const double *buffer )
 -- Purpose:
@@ -73,12 +91,27 @@ makeDatasetDouble loc dsname dims buffer =
 
 
 
+-- * HDF5 image operations
+-- The HDF5 Image API defines a standard storage for HDF5 datasets that are intended to be interpreted as images. The specification for this API is presented in another document: HDF5 Image and Palette Specification. This version of the API is primarily concerned with two dimensional raster data similar to HDF4 Raster Images. The HDF5 image API uses the Lite HDF5 API. 
+
+-- herr_t H5IMread_image ( hid_t loc_id, const char *dset_name, unsigned char *buffer ) 
+-- Purpose:
+--     Reads image data from disk. 
+-- Description:
+--     H5IMread_image reads a dataset named dset_name attached to the file or group specified by the identifier loc_id. 
+-- Parameters:
+--     hid_t loc_id 	IN: Identifier of the file or group to read the dataset from.
+--     const char *dset_name 	IN: The name of the dataset.
+--     unsigned char *buffer     	OUT: Buffer with data to store the image.
+-- Returns:
+--     Returns a non-negative value if successful; otherwise returns a negative value.
+
+readImage locId name = undefined
+
+
+
+
 -- | Helpers
-
-
-
-
-
 
 class Sized v where
   type SizedData v :: * 
