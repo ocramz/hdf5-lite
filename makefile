fec162eeb9c3e425602506af6677fe8ffb447b71
ocramz <zocca.marco@gmail.com>
2018-03-29 16:27:31 +0200

    upd makefile

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
+all:
+	make c2hs
+	stack build
+
 c2hs:
 	c2hs --cppopts='-I/usr/local/hdf5/include/' src/Data/HDF5/Internal/Types.chs
--- src/Data/HDF5/Lite.hs
+++ src/Data/HDF5/Lite.hs
@@ -17,8 +17,23 @@ C.context H.hdf5ctx
 C.include "<hdf5_hl.h>"
 
 -- herr_t H5LTmake_dataset ( hid_t loc_id, const char *dset_name, int rank, const hsize_t *dims, hid_t type_id, const void *buffer )
--- makeDataset
---   :: Hid -> String -> CInt -> Ptr Hsize -> Hid -> Ptr () -> IO Herr
+-- Purpose:
+--     Creates and writes a dataset of a type type_id.      
+-- Description:
+--     H5LTmake_dataset creates and writes a dataset named dset_name attached to the object specified by the identifier loc_id.
+
+--     The parameter type_id can be any valid HDF5 predefined native datatype; For example, setting type_id to H5T_NATIVE_INT will result in a dataset of signed integer datatype.
+-- Parameters:
+--     hid_t loc_id 	IN: Identifier of the file or group to create the dataset within.
+--     const char *dset_name    	IN: The name of the dataset to create.
+--     int rank 	IN: Number of dimensions of dataspace.
+--     const hsize_t * dims 	IN: An array of the size of each dimension.
+--     hid_t type_id 	IN: Identifier of the datatype to use when creating the dataset.
+--     const void * buffer 	IN: Buffer with data to be written to the dataset.
+-- Returns:
+--     Returns a non-negative value if successful; otherwise returns a negative value. 
+makeDataset
+  :: Hid -> String -> CInt -> Ptr Hsize -> Hid -> Ptr () -> IO Herr
 makeDataset loc dsname rank dims tyid buffer = withCString dsname $ \dsname_ -> 
   [C.exp| herr_t{
       H5LTmake_dataset( $(hid_t loc), $(const char* dsname_), $(int rank), $(const hsize_t* dims), $(hid_t tyid), $(const void* buffer) )
