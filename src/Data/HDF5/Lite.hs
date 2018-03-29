3891fe2444c6e6f7dd18a31a12f08b85c57d6cba
ocramz <zocca.marco@gmail.com>
2018-03-29 19:05:06 +0200

    adding file operations

--- src/Data/HDF5/Lite.hs
+++ src/Data/HDF5/Lite.hs
@@ -8,11 +8,11 @@ import Control.Monad.Catch (throwM, MonadThrow(..))
 import Control.Exception (bracket)
 
 import Foreign.C.String (peekCString, withCString)
-import Foreign.C.Types
-import Foreign.Marshal.Alloc (alloca)
+import Foreign.C.Types (CInt)
+-- import Foreign.Marshal.Alloc (alloca)
 import Foreign.Marshal.Array (withArray)
-import Foreign.Ptr (Ptr)
-import Foreign.Storable
+-- import Foreign.Ptr (Ptr)
+-- import Foreign.Storable
 
 import GHC.Prim
 
@@ -36,13 +36,36 @@ C.include "<hdf5_hl.h>"
 
 -- | HDF5 Lite API reference: https://support.hdfgroup.org/HDF5/doc/HL/RM_H5LT.html
 
-
-
+-- hid_t H5Fcreate( const char *name, unsigned flags, hid_t fcpl_id, hid_t fapl_id )
+-- Purpose:
+--     Creates an HDF5 file.
+-- Description:
+--     H5Fcreate is the primary function for creating HDF5 files; it creates a new HDF5 file with the specified name and property lists and specifies whether an existing file of same name should be overwritten.
+--     The name parameter specifies the name of the new file.
+--     The flags parameter specifies whether an existing file is to be overwritten. It should be set to either H5F_ACC_TRUNC to overwrite an existing file or H5F_ACC_EXCL, instructing the function to fail if the file already exists.
+--     New files are always created in read-write mode, so the read-write and read-only flags, H5F_ACC_RDWR and H5F_ACC_RDONLY, respectively, are not relevant in this function. Further note that a specification of H5F_ACC_RDONLY will be ignored; the file will be created in read-write mode, regardless.
+--     More complex behaviors of file creation and access are controlled through the file creation and file access property lists, fcpl_id and fapl_id, respectively. The value of H5P_DEFAULT for any property list value indicates that the library should use the default values for that appropriate property list.
+--     The return value is a file identifier for the newly-created file; this file identifier should be closed by calling H5Fclose when it is no longer needed. 
+fcreate :: String -> IO Hid
+fcreate name = withCString name $ \name_ ->
+  [C.exp| hid_t{ H5Fcreate( $(const char* name_), H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT)}|]
 
 
 
 -- * HDF5 file operations
 -- NB: each HDF5 physical file (e.g. "/usr/data/dataset0.h5" ) specifies a whole internal filesystem, the elements of which are what we call "files" here
+
+-- hid_t H5Fopen( const char *name, unsigned flags, hid_t fapl_id )
+-- Purpose:
+--     Opens an existing HDF5 file.
+-- Description:
+--     H5Fopen is the primary function for accessing existing HDF5 files. This function opens the named file in the specified access mode and with the specified access property list.
+--     Note that H5Fopen does not create a file if it does not already exist; see H5Fcreate.
+--     The name parameter specifies the name of the file to be opened.
+--     The fapl_id parameter specifies the file access property list. Use of H5P_DEFAULT specifies that default I/O access properties are to be used
+--     The flags parameter specifies whether the file will be opened in read-write or read-only mode, H5F_ACC_RDWR or H5F_ACC_RDONLY, respectively. More complex behaviors of file access are controlled through the file-access property list.
+--     The return value is a file identifier for the open file; this file identifier should be closed by calling H5Fclose when it is no longer needed. 
+
 
 -- hid_t H5Fopen( const char *name, unsigned flags, hid_t fapl_id )
 fopenReadOnly :: String -> IO Hid
@@ -52,6 +75,35 @@ fopenReadOnly name = withCString name $ \name_ ->
 fopenRW :: String -> IO Hid
 fopenRW name = withCString name $ \name_ ->
   [C.exp| hid_t{ H5Fopen( $(const char* name_), H5F_ACC_RDWR, H5P_DEFAULT )}|]
+
+data FMode = ReadOnly | ReadWrite deriving (Eq, Show)
+
+fopen :: String -> FMode -> IO Hid
+fopen name mode =
+  case mode of ReadOnly -> fopenReadOnly name
+               ReadWrite -> fopenRW name
+
+
+-- herr_t H5Fclose( hid_t file_id )
+-- Purpose:
+--     Terminates access to an HDF5 file.
+-- Description:
+--     H5Fclose terminates access to an HDF5 file by flushing all data to storage and terminating access to the file through file_id.
+--     If this is the last file identifier open for the file and no other access identifier is open (e.g., a dataset identifier, group identifier, or shared datatype identifier), the file will be fully closed and access will end.
+--     Delayed close:
+--     Note the following deviation from the above-described behavior. If H5Fclose is called for a file but one or more objects within the file remain open, those objects will remain accessible until they are individually closed. Thus, if the dataset data_sample is open when H5Fclose is called for the file containing it, data_sample will remain open and accessible (including writable) until it is explicitely closed. The file will be automatically closed once all objects in the file have been closed.
+--     Be warned, however, that there are circumstances where it is not possible to delay closing a file. For example, an MPI-IO file close is a collective call; all of the processes that opened the file must close it collectively. The file cannot be closed at some time in the future by each process in an independent fashion. Another example is that an application using an AFS token-based file access privilage may destroy its AFS token after H5Fclose has returned successfully. This would make any future access to the file, or any object within it, illegal.
+--     In such situations, applications must close all open objects in a file before calling H5Fclose. It is generally recommended to do so in all cases.
+-- Parameters:
+--     hid_t file_id     	IN: Identifier of a file to terminate access to.
+fclose :: Hid -> IO Herr
+fclose fid = [C.exp| herr_t{ H5Fclose( $(hid_t fid))}|]
+
+
+-- | File memory bracket
+withFile :: String -> FMode -> (Hid -> IO c) -> IO c
+withFile name mode = bracket (fopen name mode) fclose
+
 
 
 
@@ -76,6 +128,7 @@ fopenRW name = withCString name $ \name_ ->
 --     IN: Buffer with data to be written to the dataset.
 -- Returns:
 --     Returns a non-negative value if successful; otherwise returns a negative value.
+makeDatasetDouble :: Hid -> String -> [Hsize] -> VS.Vector C.CDouble -> IO Herr
 makeDatasetDouble loc dsname dims buffer =
   let
     rank = fromIntegral $ length dims
@@ -106,7 +159,7 @@ makeDatasetDouble loc dsname dims buffer =
 -- Returns:
 --     Returns a non-negative value if successful; otherwise returns a negative value.
 
-readImage locId name = undefined
+-- readImage locId name = undefined
 
 
 
